d323a1e62dd2420937df982bdde4b915e0f7d390
diana <dianakieru1@gmil.com>
2022-03-13 15:20:57 +0300

    styled my project background

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-let description=document.querySelectorAll(".description")
-let icon = document.querySelectorAll(".icon")
-let tools = document.querySelector(".design")
-tools.addEventListener("click", (e)=>{
-    e.preventDefault()
-    if(description[0].style.display=="none"){
-        description[0].style.display="block"
-        icon[0].style.display="none"       
-    
-    }
-    else{
-        description[0].style.display="none"
-        icon[0].style.display="block" 
-    }
-    if(description[1].style.display=="none"){
-        description[1].style.display="block"
-        icon[1].style.display="none"       
-    
-    }
-    else{
-        description[1].style.display="none"
-        icon[1].style.display="block" 
-    }
-    if(description[2].style.display=="none"){
-        description[2].style.display="block"
-        icon[2].style.display="none"       
-    
-    }
-    else{
-        description[2].style.display="none"
-        icon[2].style.display="block" 
-    }
-    // $('h1#heading').text($(this).text());
+let description = document.querySelectorAll('.description')
+let icon = document.querySelectorAll('.icon')
+let tools = document.querySelector('.design')
+tools.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (description[0].style.display == 'none') {
+    description[0].style.display = 'block'
+    icon[0].style.display = 'none'
+  } else {
+    description[0].style.display = 'none'
+    icon[0].style.display = 'block'
+  }
+  if (description[1].style.display == 'none') {
+    description[1].style.display = 'block'
+    icon[1].style.display = 'none'
+  } else {
+    description[1].style.display = 'none'
+    icon[1].style.display = 'block'
+  }
+  if (description[2].style.display == 'none') {
+    description[2].style.display = 'block'
+    icon[2].style.display = 'none'
+  } else {
+    description[2].style.display = 'none'
+    icon[2].style.display = 'block'
+  }
+  // $('h1#heading').text($(this).text());
 })
-
